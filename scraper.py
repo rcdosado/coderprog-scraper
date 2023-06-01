d53a4e8c74f42110e2c21100d0db19923d63b513
roy cyril dosado <rcdosado@gmail.com>
2023-06-01 19:23:58 +0800

    refactors implemented

--- scraper.py
+++ scraper.py
@@ -11,6 +11,8 @@ import re
 
 from selectorlib import selectorlib
 from selectorlib import formatter
+from parsers.book_parser import parse_book
+from parsers.course_parser import parse_course
 
 ROOT_URL = "https://coderprog.com"
 
@@ -22,12 +24,6 @@ def read_file_content(filename):
     with open(os.path.join(cur_dir, "tests", "data", filename)) as fileobj:
         content = fileobj.read()
     return content
-
-
-def save_to_file(response):
-    # saving response to `response.html`
-    with open("response.html", "w") as fp:
-        fp.write(response.text)
 
 
 def save_text(fn, contents):
@@ -60,17 +56,6 @@ def _splitter(data):
     return [i.strip() for i in data.split("|")]
 
 
-def _extract_values_from_patterns(patterns, json_string):
-    # Extract values using regex patterns
-    values = {}
-    match = None
-    for key, pattern in patterns.items():
-        match = re.search(pattern, json_string)
-        if match:
-            values[key] = match.group(1)
-    return values
-
-
 # base site e.g https://coderprog.com
 def _link_generator(pages):
     yield ROOT_URL
@@ -84,7 +69,6 @@ def get_response(url):
         print("URL request to {} returns ERROR {}: ".format(url, response.status_code))
         return None
     else:
-        #  save_to_file(response)
         return response.text
 
 
@@ -102,88 +86,6 @@ def html_to_json(html):
         return None
 
 
-def parse_book(json_data):
-    title = json_data.get("item_title")
-    url = json_data.get("item_url")
-    post_date = json_data.get("item_post_date")
-
-    # Define regex patterns for each value
-    patterns = {
-        "language": r"(\w+)",
-        "year": r"(\d{4})",
-        "ISBN": r"ISBN: ([\d-]+)",
-        "pages": r"(\d+) Pages",
-        "formats": r"(PDF|EPUB|PDF,EPUB|EPUB,PDF)",
-        "size": r"(\d+ MB)",
-    }
-
-    # Extract values using regex patterns
-    values = _extract_values_from_patterns(patterns, json_data.get("item_attribs"))
-
-    language = values.get("language")
-    year = values.get("year")
-    isbn = values.get("ISBN")
-    pages = values.get("pages")
-    formats = values.get("formats")
-    size = values.get("size")
-
-    return {
-        "category": "books",
-        "title": title,
-        "url": url,
-        "posted": post_date,
-        "language": language,
-        "published": year,
-        "isbn": isbn,
-        "num_pages": pages,
-        "types_available": formats,
-        "size": size,
-    }
-
-
-def parse_tutorial(json_data):
-    title = json_data.get("item_title")
-    url = json_data.get("item_url")
-    posted = json_data.get("item_post_date")
-
-    # Define regex patterns for each value
-    patterns = {
-        "language": r"(\w+)",
-        "format": r"(MP4|MKV|MP4,MKV|MP4,MKV)",
-        "resolution": r"(AVC \w+.\w+)",
-        "audio": r"(AAC \w+.\w+)",
-        "duration": r"\|([^|]+(?i:lectures|lessons)[^|]+)\|",
-        "size": r"(\d+(?:\.\d+)? [GM]B)",
-    }
-
-    # Extract values using regex patterns
-    values = _extract_values_from_patterns(patterns, json_data.get("item_attribs"))
-
-    language = values.get("language")
-    file_type = values.get("format")
-    resolution = values.get("resolution")
-    audio = values.get("audio")
-    duration = values.get("duration").strip() if (values.get("duration")) else "None"
-    size = values.get("size")
-
-    return {
-        "category": "tutorial",
-        "title": title,
-        "url": url,
-        "posted": posted,
-        "language": language,
-        "file_type": file_type,
-        "resolution": resolution,
-        "audio": audio,
-        "duration": duration,
-        "size": size,
-    }
-
-
-def sort_based_on_timestamp(dict_list):
-    return sorted(dict_list, key=lambda x: x["timestamp"])
-
-
 def transform(json_data):
     item_list = []
     try:
@@ -193,7 +95,7 @@ def transform(json_data):
             item = (
                 parse_book(i)
                 if ("PDF" in attribs or "EPUB" in attribs)
-                else parse_tutorial(i)
+                else parse_course(i)
             )
             item_list.append(item)
         return item_list  # return a list
@@ -212,64 +114,83 @@ def scrape(url):
 
 
 def scrape_test(url):
-    response = read_file_content("sample_index_page2.html")
-    return transform(html_to_json(response))
+    response = read_file_content("sample_index_page.html")
+    jsf = html_to_json(response)
+    ts = transform(jsf)
+    return ts
 
 
-def locked_file_dump(handle, contents, lock):
-    try:
-        with lock:
-            for item in contents:
-                handle.write(json.dumps(item) + ",\n")
-            handle.flush()
-    except Exception as e:
-        # Handle any exceptions that occurred during scraping
-        print(f"An error occurred: {str(e)}")
-    return
+class FileHandler:
+    def __init__(self, filename):
+        self.filename = filename
+        self.lock = threading.Lock()
+
+    def add_opening_bracket(self):
+        self.write("[\n")
+
+    def locked_file_dump(self, contents):
+        try:
+            with self.lock:
+                with open(self.filename, "a") as file_writer:
+                    for item in contents:
+                        file_writer.write(json.dumps(item, indent=2) + ",\n")
+                    file_writer.flush()
+        except Exception as e:
+            # Handle any exceptions that occurred during scraping
+            print(f"An error occurred: {str(e)}")
+        return
+
+    def write(self, data):
+        with open(self.filename, "a") as file_writer:
+            file_writer.write(data)
+
+    def add_closing_bracket(self):
+        with open(self.filename, "r+") as file:
+            file.seek(0, 2)
+            file.seek(file.tell() - 3, 0)
+            file.truncate()
+            file.write("\n]")
 
 
 @click.command()
 @click.option(
-    "--num_pages", default=5, help="Pages from starting page, default : 5 pages"
+    "-n", "--num_pages", default=5, help="Pages from starting page, default : 5 pages"
 )
 @click.option(
-    "--max_workers", default=3, help="Worker threads to run, default: 3 workers"
+    "-m","--max_workers", default=3, help="Worker threads to run, default: 3 workers"
 )
-@click.option("--sleep_time", default=7, help="Seconds to sleep, default: 7 seconds")
-@click.option("--when_pause", default=5, help="Pause time (defaults to every 5 MOD)")
+@click.option("-s", "--sleep_time", default=7, help="Seconds to sleep, default: 7 seconds")
+@click.option("-p", "--when_pause", default=5, help="Pause time (defaults to every 5 MOD)")
 @click.option(
+    "-d",
     "--dump_file",
     default="scraped_data.json",
     help="Filename output file",
 )
 def start_scrape(num_pages, max_workers, dump_file, sleep_time, when_pause):
-    lock = threading.Lock()
-    with open(dump_file, "a") as file_writer:
-        file_writer.write("[")
-        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-            counter = 0  
-            scrape_futures = []
-            for url in _link_generator(num_pages):
-                scrape_futures.append(executor.submit(scrape, url))
-                counter += 1
-                if counter % when_pause == 0:
-                    time.sleep(sleep_time)
-            # Iterate over the completed futures
-            for future in concurrent.futures.as_completed(scrape_futures):
-                try:
-                    # Get the result of the completed future
-                    if future.done() and not future.cancelled():
-                        result = future.result()
-                        locked_file_dump(file_writer, result, lock)
-                except Exception as e:
-                    # Handle any exceptions that occurred during scraping
-                    print(f"(start_scrape) : An error occurred: {str(e)}")
-    with open(dump_file, "r+") as f:
-        f.seek(0, 2)
-        f.seek(f.tell() - 3, 0)
-        f.truncate()
-        f.write("\n]")
+    file_handler = FileHandler(dump_file)
+    file_handler.add_opening_bracket()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        counter = 0
+        scrape_futures = []
+        for url in _link_generator(num_pages):
+            scrape_futures.append(executor.submit(scrape, url))
+            counter += 1
+            if counter % when_pause == 0:
+                time.sleep(sleep_time)
+        # Iterate over the completed futures
+        for future in concurrent.futures.as_completed(scrape_futures):
+            try:
+                # Get the result of the completed future
+                if future.done() and not future.cancelled():
+                    result = future.result()
+                    file_handler.locked_file_dump(result)
+            except Exception as e:
+                # Handle any exceptions that occurred during scraping
+                print(f"(start_scrape) : An error occurred: {str(e)}")
+    file_handler.add_closing_bracket()
 
 
 if __name__ == "__main__":
     start_scrape()
+    #  scrape_test(ROOT_URL);
